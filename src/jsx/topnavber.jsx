5fbeaecb2cbe2c651a75de3afe70b0dc9ab0683c
samir Mishra <smir.mishra1551@gmail.com>
2021-02-15 21:50:04 +0545

    top-nav completed

--- src/jsx/topnavber.jsx
+++ src/jsx/topnavber.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 import '../css/top-navbar.css';
 
 export default function TopNavbar(){
     return(
         <div className="topbar-main-container">
-            <div className="logo-section">
-                <div className="logo"></div>
-            </div>
+            <div className="logo"></div>
             <div className="social-icons">
-                <FaFacebook />
-                <FaInstagram />
+                <FaFacebook className="icons-social-nav" />
+                <FaInstagram className="icons-social-nav" />
+                <FaTwitter className="icons-social-nav" />
             </div>
         </div>
     );
