dfc30a51220564b7e40e17c215b453260f049025
samir Mishra <smir.mishra1551@gmail.com>
2021-02-15 15:43:37 +0545

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from './jsx/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>trial</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+      </Switch>
+      
     </div>
+    </Router>
   );
 }
 
---
+++ src/jsx/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Home(){
+    return(
+        <div className="home-main-container">
+            lkjasd
+        </div>
+    );
+}
