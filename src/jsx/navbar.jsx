fd4365386812b2a5dfb5a05e2d66a39a7e597a17
samir Mishra <smir.mishra1551@gmail.com>
2021-02-17 03:59:08 +0545

    banner sec completed

--- src/jsx/navbar.jsx
+++ src/jsx/navbar.jsx
@@ -7,11 +7,21 @@ export default function Navbar() {
     <div className="navbar-main-container">
       <div className="background-sec">
         <div className="text-title">
-          <h1 className="title">Pitchlab</h1>
         </div>
 
         <div className="nvabar-components-main-container">
-          <div className="navbar-mid-components"></div>
+          <div className="navbar-mid-components">
+            <div className="mid-1">
+              <h1 className="pitchlab">Pitchlab</h1>
+            <div className="caption-heading">
+              <h1>Innovate better,<br/>
+              <span className="heading-color">faster.</span></h1>
+              <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, officia labore qui voluptatum eos perspiciatis quam beatae ab consequuntur vero fugit dolor iure autem delectus dicta sapiente quae, quibusdam alias.</p>
+              <Link className="get-started" to="/contact">Get Started</Link>
+            </div>
+            </div>
+            <div className="mid-2"></div>
+          </div>
           <div className="navbar-section-bottom-container">
             <div className="nav-links-container">
               <Link className="navbarlinks" to="/">
