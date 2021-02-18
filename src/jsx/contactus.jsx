aa9f6da35bdd4efdc95ee1c67ca86585fd9b344c
samir Mishra <smir.mishra1551@gmail.com>
2021-02-18 22:02:04 +0545

    contact page navbar added

--- src/jsx/contactus.jsx
+++ src/jsx/contactus.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SecondNavbar from './navbar-secondary';
 
 export default function Contact(){
     return(
         <div className="contact-main-container">
-            
+            <SecondNavbar/>
         </div>
     );
 }
--- src/jsx/navbar-secondary.jsx
+++ src/jsx/navbar-secondary.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/navbar-secondary.css';
 
 export default function SecondNavbar(){
     return(
         <div className="secondnvabar-main-container">
-            
+            <div className="logo-container">
+                <a href="https://pitchlab.net/"><div className="logo-secondary"></div></a>
+            </div>
+            <div className="nav-links-secondary">
+                <div className="navbar-links-inner">
+
+                <Link className="second-navlinks" to="/">Home</Link>
+                <Link className="second-navlinks" to="/service">Service</Link>
+                <Link className="second-navlinks" to="/career">career</Link>
+                <Link className="second-navlinks" to="/contact">Contact</Link>
+                </div>
+            </div>
         </div>
     );
 }
