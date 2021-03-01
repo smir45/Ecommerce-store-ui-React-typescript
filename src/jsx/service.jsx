47b5def3253834a4bd51216e4845453ae419fb86
samir Mishra <smir.mishra1551@gmail.com>
2021-03-02 02:06:49 +0545

    random

--- src/jsx/service.jsx
+++ src/jsx/service.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import SecondNavbar from './navbar-secondary';
 import '../css/service.css';
 
 export default function Service(){
     return(
         <div className="service-main-container">
-            
+            <SecondNavbar/>
+            <div className="background-section">
+                <div className="pitchlab-watermark"></div>
+            </div>
         </div>
     );
 }
